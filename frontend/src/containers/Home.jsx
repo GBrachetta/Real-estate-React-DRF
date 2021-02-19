6cbce90af608d242ae8b93fe006df72899cfac91
Guillermo Brachetta <brachetta@me.com>
2021-02-19 22:39:02 +0100

    Start Home and Listing Components

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import ListingForm from '../components/ListingForm';
+import Listings from '../components/Listings';
+import Pagination from '../components/Pagination';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [listings, setListings] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listingsPerPage, setListingsPerPage] = useState(3);
+  const [active, setActive] = useState(1);
+
+  const indexOfLastListing = currentPage * listingsPerPage;
+  const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+  const currentListings = listings.slice(
+    indexOfFirstListing,
+    indexOfLastListing
+  );
+
+  const visitPage = (page) => {
+    setCurrentPage(page);
+    setActive(page);
+  };
+
+  const previousNumber = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+      setActive(currentPage - 1);
+    }
+  };
+
+  const nextNumber = () => {
+    if (currentPage !== Math.ceil(listings.length / 3)) {
+      setCurrentPage(currentPage + 1);
+      setActive(currentPage + 1);
+    }
+  };
+
+  return (
+    <main className="home">
+      <HelmetProvider>
+        <Helmet>
+          <title>Realest Estate - Home</title>
+          <meta name="description" content="Realest Estate Home Page" />
+        </Helmet>
+      </HelmetProvider>
+      <section className="home__form">
+        <ListingForm setListings={setListings} />
+      </section>
+      <section className="home__listings">
+        <Listings listings={currentListings} />
+      </section>
+      <section className="home__pagination">
+        <div className="row">
+          {listings.length !== 0 ? (
+            <Pagination
+              itemsPerPage={listingsPerPage}
+              count={listings.length}
+              visitPage={visitPage}
+              previous={previousNumber}
+              next={nextNumber}
+              active={active}
+              setActive={setActive}
+            />
+          ) : null}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
