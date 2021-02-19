a7239f8e3ddb4479f483a29adad283edc46c5911
Guillermo Brachetta <brachetta@me.com>
2021-02-19 11:44:34 +0100

    Add contacts app, model, serializers, urls and views. Send emails from contact form

--- backend/realestate/settings.py
+++ backend/realestate/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "accounts",
     "realtors",
     "listings",
+    "contacts",
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,13 @@ DATABASES = {
         "HOST": "localhost",
     }
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "guillebrachetta@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
 
 AUTH_PASSWORD_VALIDATORS = [
     {
